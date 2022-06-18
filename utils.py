911f87447ad53ba1fd77b7e80b2f4b62acf23e62
johndpjr <johndpowell02@gmail.com>
2022-06-18 12:59:14 -0500

    separate tstamp_to_tstr into date and time parts

--- utils.py
+++ utils.py
@@ -2,13 +2,18 @@ from uuid import uuid4
 from datetime import datetime
 
 
-def get_uuid(self):
+def get_uuid():
     """Returns a Version 4 UUID."""
     return str(uuid4()).replace('-', '')
-    
+
+def tstamp_to_datestr(tstamp):
+    return tstamp.strftime('%x (%a)')
+
+def tstamp_to_timestr(tstamp):
+    return tstamp.strftime('%I:%M %p')
+
 def tstamp_to_tstr(tstamp):
-    """Converts a timestamp to the appropriate string for the user view"""
-    return tstamp.strftime("%x (%a) %I:%M %p")
+    return f'{tstamp_to_datestr(tstamp)} {tstamp_to_timestr(tstamp)}'
 
 def tstr_to_tstamp(tstr):
     """Converts a timestamp string to the appropriate timestamp object"""
