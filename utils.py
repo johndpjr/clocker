d1be18745a7504e4f705e0ac2e3a166111ba5b34
johndpjr <johndpowell02@gmail.com>
2022-06-18 16:08:37 -0500

    add get_time_parts to get readable time

--- utils.py
+++ utils.py
@@ -16,5 +16,17 @@ def tstamp_to_tstr(tstamp):
     return f'{tstamp_to_datestr(tstamp)} {tstamp_to_timestr(tstamp)}'
 
 def tstr_to_tstamp(tstr):
-    """Converts a timestamp string to the appropriate timestamp object"""
+    """Converts a timestamp string to the appropriate timestamp object."""
     return datetime.strptime(tstr[:-4], '%Y-%m-%dT%H:%M:%S')
+
+def get_time_parts(total_secs: int):
+    """Returns hours, minutes, and seconds from total_secs in a dictionary."""
+
+    time_parts = {}
+    minutes, seconds = divmod(total_secs, 60)
+    time_parts['seconds'] = seconds
+    hours, minutes = divmod(minutes, 60)
+    time_parts['minutes'] = minutes
+    time_parts['hours'] = hours
+
+    return time_parts
