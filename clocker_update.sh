03d45c8f37fe9930fb70683e08f5e2a9f2b5bc60
johndpjr <johndpowell02@gmail.com>
2022-07-02 10:16:21 -0500

    chmod clocker.py

---
+++ clocker_update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Pulling from remote..."
+git pull
+echo "Making clocker executable..."
+chmod +x clocker.py
--- clocker_update.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-emulate -LR zsh
-git pull
-cp clocker.py clocker
-chmod +x clocker
